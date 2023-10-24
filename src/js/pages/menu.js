71e33d0fe5cc62eabec213ab9ae31ea7d1734191
dogukan <Karademird67@gmail.com>
2023-10-24 16:43:25 +0200

    menu data

---
+++ src/js/pages/menu.js
@@ -0,0 +1,18 @@
+const bakerySelection  = {
+  Borek: "Flaky pastries with savory fillings.",
+  Simit: "Sesame-crusted bread rings.",
+  Pogaca: "Soft mini bread rolls with various fillings.",
+  Baklava: "Classic pastry with honey and nuts.",
+  Revani: "Semolina cake with sweet syrup."
+};
+
+const coffeeSelection = {
+  TurkishCoffee: "Rich and aromatic.",
+  Cappuccino: "Espresso with milk foam.",
+  Latte: "Espresso with steamed milk.",
+  IcedCoffee: "Chilled coffee with syrups.",
+  TurkishTea: "Strong, black tea."
+};
+
+
+
